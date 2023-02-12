c49b4a411ed1c83e7c827c3014e9b6a235bed87e
RyuKwanKon <rhks33rhs@gmail.com>
2023-02-12 17:34:30 +0900

    sensor bluetooth 통신 코드 정리/마무리

--- app/src/main/java/com/example/wear_os_sensor/MainActivity2.kt
+++ app/src/main/java/com/example/wear_os_sensor/MainActivity2.kt
@@ -1,7 +1,13 @@
 package com.example.wear_os_sensor
 
+import android.Manifest.permission.BLUETOOTH_CONNECT
 import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Context.*
 import android.content.Intent
+import android.content.pm.PackageManager
 
 import android.os.Bundle
 import android.hardware.Sensor;
@@ -9,9 +15,12 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager;
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.wear_os_sensor.databinding.ActivityMain2Binding
 import com.example.wear_os_sensor.util.connect.BluetoothDataSender
 import com.example.wear_os_sensor.util.connect.DataSender
+import android.app.Activity.BLUETOOTH_SERVICE as BLUETOOTH_SERVICE1
 
 
 class MainActivity2 : Activity(), SensorEventListener {
@@ -20,14 +29,28 @@ class MainActivity2 : Activity(), SensorEventListener {
     private lateinit var sensorViewModel: SensorViewModel
     private lateinit var dataSender: DataSender
 
+//    private var bluetoothManager: BluetoothManager? = null
+//    private lateinit var mBluetoothAdapter: BluetoothAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
 
+//        bluetoothManager = this.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+//        mBluetoothAdapter = bluetoothManager!!.getAdapter()
+
         sensorViewModel = SensorViewModel(getSystemService(SENSOR_SERVICE) as SensorManager, this)
         // 통신방식 Bluetooth
-        dataSender = BluetoothDataSender()
+        dataSender = BluetoothDataSender(this)
         setContentView(binding.root)
+
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)
+//            != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(activity,
+//                arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+//                REQUEST_BLUETOOTH_CONNECT);
+//        }
+
 
         // 통신 연결
         val msg1: String = dataSender.connect( this)
--- app/src/main/java/com/example/wear_os_sensor/util/connect/BlutoothDataSender.kt
+++ app/src/main/java/com/example/wear_os_sensor/util/connect/BlutoothDataSender.kt
@@ -3,6 +3,7 @@ package com.example.wear_os_sensor.util.connect
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.util.Log
@@ -13,14 +14,25 @@ import java.util.*
 
 
 class BluetoothDataSender: DataSender {
-    private var mBluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+    private var bluetoothManager: BluetoothManager? = null
+    private var mBluetoothAdapter: BluetoothAdapter
     private lateinit var pairedDvices: Set<BluetoothDevice>
     private lateinit var mBluetoothDevice: BluetoothDevice
     private lateinit var mBluetoothSocket: BluetoothSocket
     private lateinit var mOutputStream: OutputStream
 
+
+    constructor(context:Context){
+        bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = bluetoothManager!!.adapter
+        Log.d("qwe", "Zxca")
+    }
+
+
     @SuppressLint("MissingPermission")
     override fun connect(context: Context): String {
+
         pairedDvices = mBluetoothAdapter.bondedDevices
         val connected = getParingBluetoothDevice(pairedDvices)
         if (connected.equals("error")) return "error"
@@ -29,12 +41,15 @@ class BluetoothDataSender: DataSender {
 
         try {
             mBluetoothSocket = mBluetoothDevice.createRfcommSocketToServiceRecord(uuid)
+            if (mBluetoothSocket.isConnected()) Log.d("", "qweasd")
+
+            Thread.sleep(1000)
             mBluetoothSocket.connect()
             mOutputStream = mBluetoothSocket.outputStream
             return connected
         } catch (e: IOException) {
             Log.e("Bluetooth Connection", "Error opening socket", e)
-            return "error"
+            return "error123"
         }
     }
 
@@ -58,11 +73,14 @@ class BluetoothDataSender: DataSender {
 
     //페어링된 기기를 찾는 함수
     @SuppressLint("MissingPermission")
-    fun getParingBluetoothDevice(pairedDevice: Set<BluetoothDevice>): String{
+    private fun getParingBluetoothDevice(pairedDevice: Set<BluetoothDevice>): String{
         try {
-            for (bluetoothDevice: BluetoothDevice in pairedDevice)
-                if(isConnected(bluetoothDevice) as Boolean)
+            for (bluetoothDevice: BluetoothDevice in pairedDevice) {
+                if (isConnected(bluetoothDevice) as Boolean) {
+                    Log.d(bluetoothDevice.toString(), bluetoothDevice.name)
                     return bluetoothDevice.toString()
+                }
+            }
         } catch (e: IOException) {
             //블루투스 서비스 사용불가인 경우
         }
@@ -70,7 +88,7 @@ class BluetoothDataSender: DataSender {
     }
 
     // 현재 연결되어있는지 유무
-    fun isConnected(device: BluetoothDevice): Any? {
+    private fun isConnected(device: BluetoothDevice): Any? {
         val m: Method = device.javaClass.getMethod("isConnected")
         val connected = m.invoke(device)
         return connected
